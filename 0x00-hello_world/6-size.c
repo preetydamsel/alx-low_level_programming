2cc3433f64ced723e38b3ea100119442dec78e32
patience <example@you.com>
2023-03-16 10:09:25 -0700

    task6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- * main - A program that prints the size of various files
- * Return 0 (success)
+ * main - A program that prints the size of various computer types
+ * Return 0 (Success)
  */
 int main(void)
 {
@@ -13,7 +13,7 @@ int main(void)
 printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
 printf("size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
 printf("size of a long: %lu byte(s)\n", (unsigned long)sizeof(c));
-printf("size of a long long: %lu byte(s)\n", (unsigned long)sizeof(d));
+printf("size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
 printf("size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 return (0);
 }
