19c14e83b274e34f3d62c3906287484bedb6cfcb
patience <example@you.com>
2023-03-16 12:59:38 -0700

    task5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - A program that prints a piece of art
+ * main - A programm to print out a c file
  * Return 0 (Success)
  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	     return (0);
+	return (0);
 }
