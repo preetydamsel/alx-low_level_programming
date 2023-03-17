a4a73acbf559b2218778829c8568d81a30d74f75
patience <example@you.com>
2023-03-16 21:44:09 -0700

    task

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 /**
  * main - A programm to print out a file in a ddynamic style
- * Return 0 (Success)
+ * Return: 0 (Success)
  */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
