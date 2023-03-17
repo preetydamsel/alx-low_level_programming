92797be61cbb60ab2da2bc9d23b22985bacfb76d
patience <example@you.com>
2023-03-16 21:31:54 -0700

    task

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,9 @@
 #include <unistd.h>
 /**
  * main - Programm that displays unix file for this purpose
- * Return 1 (Sucess)
+ * Return: 1 (Sucess)
  */
+
 int main(void)
 {
 	write(2, "and that piece of art is useful\" -Dora kopar, 2015-10-19\n", 59);
