7a550e34a18ded81cbf4d4a2e217ca78e7ae39fb
patience <example@you.com>
2023-03-17 20:33:17 -0700

    task6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <stdlib>
+#include <stdlib.h>
 #include <time.h>
 
 /**
@@ -8,3 +8,13 @@
  */
 int main(void)
 
+{
+	int d;
+
+	for (d = '0'; d <= '9'; d++)
+	putchar(d);
+	putchar('\n');
+
+	return (0);
+
+}
