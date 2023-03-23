969ecfb5582659ccfb94984ac57e08a383df8c2a
patience <example@you.com>
2023-03-23 16:55:21 -0700

    digit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,15 +2,15 @@
 
 /**
  * _isdigit - Check if a character is a digit
- * @x: The number is to be checked
+ * @c: The number is to be checked
  * Return: 1 for a character that will be as a digit or 0 for any else
  */
 
-int _isdigit(int x)
+int _isdigit(int c)
 {
-	if (x >= 48 && x <= 57)
+	if (c >= 48 && c <= 57)
 	{
 	return (1);
 	}
-	return(1);
+	return (0);
 }
