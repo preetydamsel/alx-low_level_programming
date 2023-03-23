335ed399634df9e1893c91126b7ffd24931661cd
patience <example@you.com>
2023-03-22 22:21:11 -0700

    task12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -33,7 +33,7 @@ void print_times_table(int n)
 	else if (prod <= 99 && prod >= 10)
 	{
 	_putchar((prod / 10) + '0');
-	_putchar((prod % 100) + '0');
+	_putchar((prod % 10) + '0');
 	}
 	_putchar('\n');
 	}
