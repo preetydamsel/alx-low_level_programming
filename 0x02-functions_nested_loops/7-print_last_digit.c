5f87809b13bfdef408a9fd924bb0c35355845fa1
patience <example@you.com>
2023-03-22 21:30:05 -0700

    task

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,11 +3,10 @@
 /**
  * print_last_digit - prints the last digit of a number
  * @n: The number to be treated
- * Return : value of the last digit number
+ * Return : value of the last digit of number
  */
 
 int print_last_digit(int n)
-
 {
 	int last;
 
