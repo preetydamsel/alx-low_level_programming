c93d244e03b0ea75b76d94e34ce9c954d4b42259
patience <example@you.com>
2023-03-31 21:58:46 -0700

    task

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 
 int main(void)
-
 {
 	int n;
 	int a[5];
@@ -9,15 +8,16 @@ int main(void)
 
 	a[2] = 1024;
 	p = &n;
-
-	/*
-	 * write your line of code here ...
-	 * Remember:
-	 * -you are not allowed to use a
-	 *  - you are not allow to modify p
-	 *  - only one statement
-	 *  - you are not allowed to code anything else than this line of code
-	 */
-	/* ... so that this prints 98\n */
-	printf("a[2] = ℅\n", a[2]);
+/*
+ * write your line here ...
+ * Remember:
+ * - you are not allowed to use a
+ * - you are not allowed to modify p
+ * - only one statement
+ * - you are not allowed to code anythimg else than this line of code
+ */
+*p(p + 5) = 98;
+/* ...so that this prints 98\n */
+printf("a[2] = %d\n", a[2]);
+return (0);
 }
