6103bb86c006b38133b90668355ef9e8d8349591
patience <example@you.com>
2023-04-20 02:11:13 -0700

    task2

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,6 +1,5 @@
 #ifndef CALC_H
 #define CALC_H
-
 /**
  * struct op - A struct op.
  * @op: The operator.
@@ -9,7 +8,7 @@
 typedef struct op
 {
 	char *op;
-	int(*f) (int a, int b);
+	int (*f)(int a, int b);
 } op_t;
 
 int op_add(int a, int b);
