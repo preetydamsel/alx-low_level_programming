ef0391104999b833e744408d164396ddb7c9aa53
patience <patienceidoko68@gmail.com>
2023-12-11 21:42:58 -0800

    libydynamic files

---
+++ 0x18-dynamic_libraries/functions/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+/**
+ * _puts - prints a string
+ * @s: strings to be printed
+ * Return: void
+ */
+
+void _puts(char *s)
+{
+	puts(s);
+}
